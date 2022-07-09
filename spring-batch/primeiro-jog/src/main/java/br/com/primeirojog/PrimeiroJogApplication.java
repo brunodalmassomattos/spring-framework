dc01283ff8aed93a9f341522679b93f5224e8c8b
Bruno Dalmasso <69864476+brunodalmassomattos@users.noreply.github.com>
2022-07-09 14:06:20 -0300

    Testando

---
+++ spring-batch/primeiro-jog/src/main/java/br/com/primeirojog/PrimeiroJogApplication.java
@@ -0,0 +1,12 @@
+package br.com.primeirojog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroJogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroJogApplication.class, args);
+	}
+}
---
+++ spring-batch/primeiro-jog/src/test/java/br/com/primeirojog/PrimeiroJogApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.primeirojog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroJogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
