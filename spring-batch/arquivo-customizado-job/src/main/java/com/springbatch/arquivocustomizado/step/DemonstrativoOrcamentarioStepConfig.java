b4bc3992dbb1052cf1042362c0b3c933f31be07e
Bruno Dalmasso <bruno.mdalmasso@gmail.com>
2022-07-15 10:48:52 -0300

    Escrita em banco de dados

--- spring-batch/arquivo-customizado-job/src/main/java/com/springbatch/arquivocustomizado/step/DemonstrativoOrcamentarioStepConfig.java
+++ spring-batch/arquivo-customizado-job/src/main/java/com/springbatch/arquivocustomizado/step/DemonstrativoOrcamentarioStepConfig.java
@@ -2,7 +2,6 @@ package com.springbatch.arquivocustomizado.step;
 
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.file.MultiResourceItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
